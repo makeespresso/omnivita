bc9a1689f55f97f6d6efe43cd4e6378225a77bdc
Makeespresso <makeespresso@gmail.com>
2019-09-10 16:40:28 -0500

    updates on content component. Responsive D

--- omnivitatiendanaturista/src/components/Content.jsx
+++ omnivitatiendanaturista/src/components/Content.jsx
@@ -5,13 +5,13 @@ export default function Content() {
   return (
     <div className="content">
       <div className="content-image">
-        <img src={contentImage} alt="Exercising" />
+        <img className="exercising-picture" src={contentImage} alt="Exercising" />
       </div>
       <div className="content-text">
         <p>OmniVita es una tienda naturista dedicada a la venta al por menor de productos de la  salud, nutrición y belleza. Incluyendo vitaminas, suplementos minerales, hierbas, nutrición deportiva, de dieta,  productos energéticos y de belleza.</p>
-        <br />
+
         <p>Nuestros  productos son de alta calidad al alcance de la comunidad.</p>
-        <br />
+
         <p>Nos esmeramos por ofrecer productos legítimos al mejor precio para el cuidado de la salud y la belleza.</p>
       </div>
     </div>
