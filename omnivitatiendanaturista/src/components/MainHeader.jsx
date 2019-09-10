900840de793cfa1c497a66bde1a55813daf5109f
Makeespresso <makeespresso@gmail.com>
2019-09-09 22:44:48 -0500

    styling on content component

--- omnivitatiendanaturista/src/components/MainHeader.jsx
+++ omnivitatiendanaturista/src/components/MainHeader.jsx
@@ -6,6 +6,7 @@ export default function MainHeader() {
     <div className="main-container">
       <div className="hero-image-container">
         {/* <img className="hero-image" src={Heroimage} alt="Omnivita" /> */}
+        <img className="hero-image" src="https://images.unsplash.com/photo-1548302966-a78e6d439912?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1651&q=80" />
       </div>
       <div className="banner">
         <p>Productos naturales de alta calidad para el cuidado de la salud.</p>
