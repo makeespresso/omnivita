5b3153ee6e4f24dcf13c259a92b44044fb0cb7b4
Makeespresso <makeespresso@gmail.com>
2019-09-14 20:33:53 -0500

    C Component fixed div positions

--- omnivitatiendanaturista/src/App.js
+++ omnivitatiendanaturista/src/App.js
@@ -11,9 +11,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <Navbar /> */}
+        {/* <Navbar />
         <MainHeader />
-        <Content />
+        <Content /> */}
         <ProductsInfo />
         {/* <Contact /> */}
         {/* <Footer /> */}
--- omnivitatiendanaturista/src/components/MainHeader.jsx
+++ omnivitatiendanaturista/src/components/MainHeader.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-import Heroimage from '../assets/img/hero-img-omnivita.png';
+// import Heroimage from '../assets/img/hero-img-omnivita.png';
+import Heroimages from '../assets/img/vitamins-2.jpeg';
 
 export default function MainHeader() {
   return (
     <div className="main-container">
       <div className="hero-image-container">
         {/* <img className="hero-image" src={Heroimage} alt="Omnivita" /> */}
-        <img className="hero-image" src="https://images.unsplash.com/photo-1548302966-a78e6d439912?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1651&q=80" />
+        {/* <img className="hero-image" src="https://images.unsplash.com/photo-1548302966-a78e6d439912?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1651&q=80" /> */}
+        <img className="hero-image" src={Heroimages} alt="Omnivita" />
+
       </div>
       <div className="banner">
         <p>Productos naturales de alta calidad para el cuidado de la salud.</p>
