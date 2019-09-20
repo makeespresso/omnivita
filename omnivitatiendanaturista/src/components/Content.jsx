208b04e3cb57120910049c6c6a882a0dd1114c62
Makeespresso <makeespresso@gmail.com>
2019-09-19 20:03:39 -0500

    Content Component up to date

--- omnivitatiendanaturista/src/components/Content.jsx
+++ omnivitatiendanaturista/src/components/Content.jsx
@@ -16,24 +16,25 @@ export default function Content() {
         <p>Nos esmeramos por ofrecer productos legítimos al mejor precio para el cuidado de la salud y la belleza.</p>
       </div> */}
       <div class="cell cell-1">
+        <p><span>OmniVita Tienda Naturista</span> <br></br><br></br>dedicada a la venta al por menor de productos de la  salud, nutrición y belleza. Incluyendo vitaminas, suplementos minerales, hierbas, nutrición deportiva, de dieta,  productos energéticos y de belleza.</p>
+      </div>
+
+      <div class="cell cell-2">
+
         <div className="content-image">
           <img className="content-image" src={vitaminPills} alt="Products" />
         </div>
       </div>
 
-      <div class="cell cell-2">
-        <p>OmniVita es una tienda naturista dedicada a la venta al por menor de productos de la  salud, nutrición y belleza. Incluyendo vitaminas, suplementos minerales, hierbas, nutrición deportiva, de dieta,  productos energéticos y de belleza.</p>
-      </div>
-
       <div class="cell cell-3">
-        <p>Nuestros  productos son de alta calidad al alcance de la comunidad.<br /><br />
-          Nos esmeramos por ofrecer productos legítimos al mejor precio para el cuidado de la salud y la belleza.</p>
+        <img className="content-image" src={productsImage} alt="Products" />
       </div>
 
       <div class="cell cell-4">
         <div className="content-image">
+          <p>Nuestros  productos son de alta calidad al alcance de la comunidad.<br /><br />
+            Nos esmeramos por ofrecer productos legítimos al mejor precio para el cuidado de la salud y la belleza.</p>
 
-          <img className="content-image" src={productsImage} alt="Products" />
         </div>
       </div>
 
