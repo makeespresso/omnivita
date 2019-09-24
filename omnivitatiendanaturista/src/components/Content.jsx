d4d9e33fab517362eac87ed3e3bb8750118f277d
Makeespresso <makeespresso@gmail.com>
2019-09-24 17:34:00 -0500

    content component up to date

--- omnivitatiendanaturista/src/components/Content.jsx
+++ omnivitatiendanaturista/src/components/Content.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import contentImage from '../assets/img/content-image.png';
-import exercisingImage from '../assets/img/yoga.jpeg';
 import productsImage from '../assets/img/productsImage.jpeg';
 import vitaminPills from '../assets/img/vitamin-pills.jpeg'
 
@@ -10,26 +8,20 @@ export default function Content() {
       <div class="cell cell-1">
         <p><span>OmniVita Tienda Naturista</span> <br></br><br></br>Dedicada a la venta al por menor de productos de la salud, nutrición y belleza. Incluyendo vitaminas, suplementos minerales, hierbas, nutrición deportiva, dietéticos, productos energéticos y de belleza.</p>
       </div>
-
       <div class="cell cell-2">
-
         <div className="content-image">
           <img className="content-image" src={vitaminPills} alt="Products" />
         </div>
       </div>
-
       <div class="cell cell-3">
         <img className="content-image" src={productsImage} alt="Products" />
       </div>
-
       <div class="cell cell-4">
         <div className="content-image">
           <p>Nuestros  productos son de alta calidad al alcance de la comunidad.<br /><br />
             Nos esmeramos por ofrecer productos legítimos al mejor precio para el cuidado de la salud y la belleza.</p>
-
         </div>
       </div>
-
     </div >
   )
 }
