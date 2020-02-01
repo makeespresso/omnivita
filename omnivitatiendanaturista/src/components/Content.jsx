4841eeca3114a5df689cef7ffa70fe41a188663a
Makeespresso <makeespresso@gmail.com>
2020-02-01 16:32:40 -0600

    Refact. content component

--- omnivitatiendanaturista/src/components/Content.jsx
+++ omnivitatiendanaturista/src/components/Content.jsx
@@ -13,11 +13,12 @@ export default function Content() {
         <div className="content-text">
           <section className="textwrap">
             <h1>OmniVita Tienda Naturista</h1>
+            <span>---------------</span>
             <p>Dedicada a la venta al por menor de productos de la salud, nutrición y belleza. Incluyendo vitaminas, suplementos minerales, hierbas, nutrición deportiva, dietéticos, productos energéticos y de belleza.</p>
             <p>Nuestros  productos son de alta calidad al alcance de la comunidad. Nos esmeramos por ofrecer productos legítimos al mejor precio para el cuidado de la salud y la belleza.</p>
           </section>
         </div>
-        
+
       </div>
     </section>
   )
