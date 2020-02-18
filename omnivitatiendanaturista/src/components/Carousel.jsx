78564d9816e173b4b0f4e954ac2c7af7bc5e3d3c
Makeespresso <makeespresso@gmail.com>
2020-02-17 22:06:11 -0700

    Navbar Component, id's added

--- omnivitatiendanaturista/src/components/Carousel.jsx
+++ omnivitatiendanaturista/src/components/Carousel.jsx
@@ -2,35 +2,26 @@ import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import Heroimages from '../assets/img/coverimage.jpeg';
 
-function PicCarousel() {
+function FunCarousel() {
   return (
     <div className="tester">
       < Carousel >
         <Carousel.Item>
           <img className="carouselImg" src={Heroimages} alt="Omnivita" />
           <Carousel.Caption>
-            {/* <section className="backtext">
-              <h4>Omnivita Tienda Naturista</h4>
-              <p>Productos Naturales</p>
-            </section> */}
+
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img className="carouselImg" src="https://images.unsplash.com/photo-1579722820308-d74e571900a9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" />
           <Carousel.Caption>
-            {/* <section className="backtext">
-              <h4>Omnivita Tienda Naturista</h4>
-              <p>Productos Naturales</p>
-            </section> */}
+
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img className="carouselImg" src="https://images.unsplash.com/photo-1535585209827-a15fcdbc4c2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" />
           <Carousel.Caption>
-            {/* <section className="backtext">
-              <h4>Omnivita Tienda Naturista</h4>
-              <p>Productos Naturales</p>
-            </section> */}
+
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel >
@@ -41,4 +32,4 @@ function PicCarousel() {
   )
 }
 
-export default PicCarousel;
+export default FunCarousel;
